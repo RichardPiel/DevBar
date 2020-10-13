a18e29aa3a4c5b9f9f461688f8f4c2aa4cbb07ca
Richard PIEL <RPL@monvillagenutrition.com>
2020-10-13 17:25:07 +0200

    Check if devbar already injected in body

--- src/Middleware/BarMiddleware.php
+++ src/Middleware/BarMiddleware.php
@@ -39,13 +39,22 @@ class BarMiddleware
         
         $body->rewind();
         $contents = $body->getContents();
+
         $pos = strrpos($contents, '</body>');
         if ($pos === false) {
             return $response;
         }
+
+        // DevBar already injected ?
+        $pos = strrpos($contents, 'id="devbar"');
+        if ($pos != false) {
+            return $response;
+        }
+
         $body->rewind();
-        $development_bar = '<div style="width:100%;padding:2px 10px;background-color: #e63757;position: absolute;top: 0;left: 0;text-align:right;color:white"><strong>Attention :</strong> Le Debug actif !</div>';
+        $development_bar = '<div id="devbar" style="width:100%;padding:2px 10px;background-color: #e63757;position: absolute;top: 0;left: 0;text-align:right;color:white"><strong>Attention :</strong> Le Debug actif !</div>';
         $contents = substr($contents, 0, $pos) . $development_bar . substr($contents, $pos);
+        $body->rewind();
 
         $body->write($contents);
         return $response->withBody($body);
